711cc2f96ecb890dca8617adc6123e2de3ed0d6c
Briyo1 <briyotechnologies01@gmail.com>
2026-09-18 16:49:52 +0530

    config changes

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
 
-   output: "export",
+  trailingSlash: true,
+
   images: {
     unoptimized: true,
   },
+
   basePath: "/zioza-connect-frontend",
   assetPrefix: "/zioza-connect-frontend/",
-
 };
 
 export default nextConfig;
